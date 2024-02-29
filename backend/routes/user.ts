7d9aba99ed5dac2774ec3462e92a75426d35af93
Vishesh Pandey <74998585+Vishesh-Pandey@users.noreply.github.com>
2024-02-29 10:58:06 +0530

    added login route with jsonwebtoken

--- backend/routes/user.ts
+++ backend/routes/user.ts
@@ -1,6 +1,7 @@
 import zod from "zod";
 import { Router } from "express";
 import { PrismaClient } from "@prisma/client";
+import jwt from "jsonwebtoken";
 
 const prisma = new PrismaClient();
 
@@ -42,6 +43,41 @@ router.post("/signup", async (req, res) => {
     return res.json({
       msg: "Signup Successful",
     });
+  } catch (error) {
+    res.json({
+      msg: "Internal Server Error",
+    });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { success } = loginSchema.safeParse(req.body);
+
+    if (!success) {
+      return res.json({
+        msg: "Invalid Input",
+      });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        username: req.body.username,
+        password: req.body.password,
+      },
+    });
+
+    if (user === null) {
+      return res.json({
+        msg: "Incorrect Credentials",
+      });
+    } else {
+      const token = jwt.sign(
+        { username: req.body.username },
+        process.env.JWT_SECRET || "invalid_secret"
+      );
+      return res.json(token);
+    }
   } catch (error) {
     res.json({
       msg: "Internal Server Error",
